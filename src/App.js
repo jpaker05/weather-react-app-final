d9cf0355a2acee5644447a02cdfafa48427f2f33
jpaker05 <110342564+jpaker05@users.noreply.github.com>
2023-01-24 12:34:45 -0600

    add github repo url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Jessica Paker and is {""}
+        <a
+          href="https://github.com/jpaker05/weather-react-app-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
