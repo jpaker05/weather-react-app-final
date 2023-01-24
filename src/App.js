31a6b848993f271d48a9fa303a79b27c3522bd12
jpaker05 <110342564+jpaker05@users.noreply.github.com>
2023-01-24 12:54:08 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Jessica Paker and is {""}
-        <a
-          href="https://github.com/jpaker05/weather-react-app-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Jessica Paker and is {""}
+          <a
+            href="https://github.com/jpaker05/weather-react-app-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
